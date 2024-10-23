b99c81c99b6f79599f5979341048f00f730ee030
HebePython <50890910+HebePython@users.noreply.github.com>
2024-10-23 16:13:22 +0200

    added imports of all zone classes

--- main/Main.java
+++ main/Main.java
@@ -2,6 +2,10 @@ package main;
 
 import main.Worlds.World;
 import main.Worlds.Zone1;
+import main.Worlds.Zone2;
+import main.Worlds.Zone3;
+import main.Worlds.Zone4;
+import main.Worlds.Zone5;
 
 public class Main {
 
@@ -9,8 +13,10 @@ public class Main {
         World world = new World();
 
         Zone1 zone1 = new Zone1();
+        Zone2 zone2 = new Zone2();
         System.out.println(zone1.getZoneName());
         zone1.displayCurrentZone();
+        zone2.displayCurrentZone();
     }
     
 }
